2c7d766b46336da98d4d5a1754b8163201d497ac
Inochi <81598926+StayAIive@users.noreply.github.com>
2021-08-17 19:46:28 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var arr =[1,2,3,4,5,6,7,8,9,10];
+
+function find_num(arr,x)
+{
+   for(var i = 0; i < arr.length; i++){
+     if(x == arr[i]){
+       return i;
+     }
+   }
+   return -1;
+}
+function setup() 
+{
+  createCanvas(400, 400);
+
+  var result = find_num(arr,5);
+  if(result == -1){
+    console.log("number not found");
+  }
+  else{
+    console.log("number found in index " + result); 
+  }
+}
+
+function draw() 
+{
+   background(220);
+  
+}
